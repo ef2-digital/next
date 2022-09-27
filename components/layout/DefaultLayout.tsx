9c027a3caa01644c9cf17b97e7d719efcce40e4d
bitbucket-bartkoelewijnef2 <bartkoelewijn@ef2.nl>
2022-09-27 09:41:41 +0200

    Navigiation import

--- components/layout/DefaultLayout.tsx
+++ components/layout/DefaultLayout.tsx
@@ -1,13 +1,14 @@
 import { ThemeProvider } from '@ef2-digital/react';
 import Footer from 'components/Footer';
+import Navigation from 'components/Navigation';
 import { PropsWithChildren } from 'react';
 
 interface DefaultLayoutProps {}
 
 const DefaultLayout = ({ children }: PropsWithChildren<DefaultLayoutProps>) => {
-    // Render.
     return (
         <ThemeProvider>
+            <Navigation />
             <main id="main">{children}</main>
             <Footer />
         </ThemeProvider>
