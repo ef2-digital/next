1771abe92db4bfdd24f2e29d0702404852faf80c
bitbucket-bartkoelewijnef2 <bartkoelewijn@ef2.nl>
2022-10-06 10:03:54 +0200

    GraphQL added.

--- components/layout/DefaultLayout.tsx
+++ components/layout/DefaultLayout.tsx
@@ -1,18 +1,22 @@
+import { ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from '@ef2-digital/react';
 import Footer from 'components/Footer';
 import Navigation from 'components/Navigation';
 import { PropsWithChildren } from 'react';
 import theme from 'theme';
+import client from 'graphql/client'
 
 interface DefaultLayoutProps {}
 
 const DefaultLayout = ({ children }: PropsWithChildren<DefaultLayoutProps>) => {
     return (
-        <ThemeProvider theme={theme}>
-            <Navigation />
-            <main id="main">{children}</main>
-            <Footer />
-        </ThemeProvider>
+        <ApolloProvider client={client}>
+            <ThemeProvider theme={theme}>
+                <Navigation />
+                <main id="main">{children}</main>
+                <Footer />
+            </ThemeProvider>
+        </ApolloProvider>
     );
 };
 
