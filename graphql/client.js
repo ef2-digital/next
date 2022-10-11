9585040345b753f2c97926edb25877f4e206584b
bitbucket-bartkoelewijnef2 <bartkoelewijn@ef2.nl>
2022-10-11 10:08:35 +0200

    Client caching options

--- graphql/client.js
+++ graphql/client.js
@@ -17,9 +17,24 @@ const httpLink = new HttpLink({
     uri: `${process.env.NEXT_PUBLIC_STRAPI_URL}/graphql`
 });
 
+const defaultOptions =
+    process.env.NODE_ENV === 'development'
+        ? {
+              watchQuery: {
+                  fetchPolicy: 'no-cache',
+                  errorPolicy: 'ignore'
+              },
+              query: {
+                  fetchPolicy: 'no-cache',
+                  errorPolicy: 'all'
+              }
+          }
+        : undefined;
+
 const client = new ApolloClient({
     link: from([errorLink, httpLink]),
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    defaultOptions
 });
 
 export default client;
